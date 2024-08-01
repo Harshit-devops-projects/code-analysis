c582e95760b3a2afc6393e8df5ac1abd5a9b40a4
Harshit Bisht <Harshit.bisht.22@gmail.com>
2024-08-01 19:10:02 +0530

    code analysis

--- src/components/FlipWordsDemo.tsx
+++ src/components/FlipWordsDemo.tsx
@@ -1,14 +1,13 @@
 import React from "react";
-import { FlipWords } from "./ui/flip-words";// Ensure the correct import path
+import { FlipWords } from "./ui/flip-words"; // Ensure the correct import path
 
 export const FlipWordsDemo: React.FC = () => {
-  const words = [ "Deploy","Efficient", "Reliable", "aRutomated", "Scalable"];
+  const words = ["Analyze", "Secure", "Improve", "Measure", "Comply"];
 
   return (
     <div className="h-[40rem] flex justify-center items-center px-4">
       <div className="text-4xl mx-auto font-normal text-neutral-600 dark:text-neutral-400">
-   
-        <span><FlipWords words={words} />workflows with GitHub Actions</span>
+        <span><FlipWords words={words} /> code with SonarQube</span>
       </div>
     </div>
   );
